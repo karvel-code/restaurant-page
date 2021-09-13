0d053fb4ea99682b5f14940d23e552afa456316b
karvel-code <velgithinji@gmail.com>
2021-09-13 16:37:52 +0300

    styling for image and text

--- src/main_page.js
+++ src/main_page.js
@@ -6,12 +6,14 @@ export function mainPage(content) {
     content.appendChild(head)
 
     const container = document.createElement('div')
+    container.classList = 'rest-cont'
 
     const img = document.createElement('img')
     img.src= restaurant
     container.appendChild(img)
 
     const text = document.createElement('p')
+    text.classList = 'img-text'
     text.innerText = "The Restaurant is a high class Restaurant located in the leafy surburbs of Lavington, Nairobi - Kenya. The Restaurant offers affordable meals and drinks at an affordable price while still maintaining high quality standards of food as well as services. Be sure to visit us!"
     container.appendChild(text)
     content.appendChild(container)
