ca9480dad3140a85b245a908caba7a5356f24bc7
karvel-code <velgithinji@gmail.com>
2021-09-13 15:56:42 +0300

    Add export and import functions for main page

---
+++ src/main_page.js
@@ -0,0 +1,16 @@
+function mainPage() {
+    const head = document.createElement('h1');
+    head.innerText = "The Restaurant"
+    content.appendChild(head)
+
+    const img = document.createElement('img')
+    img.src= restaurant
+    content.appendChild(img)
+
+    const text = document.createElement('p')
+    text.innerText = "The Restaurant is a high class Restaurant located in the leafy surburbs of Lavington, Nairobi - Kenya. The Restaurant offers affordable meals and drinks at an affordable price while still maintaining high quality standards of food as well as services. Be sure to visit us!"
+    content.appendChild(text)
+
+}
+
+export {mainPage};
