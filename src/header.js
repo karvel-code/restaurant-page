808ed662f21e18b0256bda967e6a738bec0e2bb1
karvel-code <velgithinji@gmail.com>
2021-09-16 09:47:48 +0300

    Modify header

--- src/header.js
+++ src/header.js
@@ -1,14 +1,15 @@
 export function header() {
     const container = document.createElement('div')
+    container.classList = 'navigation'
     const homeLink = document.createElement('a')
     homeLink.innerText = "Home"
     homeLink.id = "home"
     const menuLink = document.createElement('a')
     menuLink.innerText = "Menu"
-    menuLink.id = "Menu"
+    menuLink.id = "menu"
     const aboutLink = document.createElement('a')
     aboutLink.innerText = "AboutUs"
-    aboutLink.id = "AboutUs"
+    aboutLink.id = "about"
     container.append(homeLink, menuLink, aboutLink)
 
     return container;
